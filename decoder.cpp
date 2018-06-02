f651ba68c4c58c44cab7e42c9ebdfa3940906bfc
mmargin <mmargin@umich.edu>
2018-06-01 20:08:24 -0400

    working on same color algorithm

--- decoder.cpp
+++ decoder.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <vector>
-#include  <iostream>
+#include <iostream>
+#include <cassert>
+#include <map>
 using namespace std;
 
 struct Guess {
@@ -18,29 +20,44 @@ vector<char> createColorVector() {
     colors.push_back('p');
     return colors;
 }
-void sameColor(const vector<char> & colors) {
-    string code;
-    int r = 0;
-    int w = 0;
-    for (int i = 0; i < colors.size(); ++i) {
-        for (int j = 0; j < 4; ++j) {
-            code += colors[i];
-        }
-        cout << code;
-        takeInput(r, w);
-    }
-}
-bool checkValid(int red, int white) {
-    return (red >= 0 && white >= 0 && red + white <= 4);
-}
 void takeInput(int & red, int & white) {
     char misc;
     cout << "Enter the red value followed by a comma and then the white value!" 
          << endl;
     cin >> red;
+    assert(red >= 0 && red <= 4);
     cin >> misc;
     cin >> white;
+    assert(white >= 0 && white <= 4);
 }
+void sameColor(const vector<char> & colors) {
+    // byrg
+    map<char,int> codeColors;
+    for (int i = 0; i < colors.size(); ++i) {
+        string code(4, colors[i]);
+        // rrrr
+        // yyyy
+        cout << code << endl;
+        int r = 0;
+        int w = 0;
+        takeInput(r, w);
+        // 1,0
+        // r = 1 w = 0
+        assert(w == 0);
+        if (r == 4) {
+            break;
+        }
+        else if (r > 0) {
+            codeColors[colors[i]] = r;
+        }
+    }
+    // what do we know? we know how many of each color in the code
+    // map has counts of distinct colors
+}
+bool checkValid(int red, int white) {
+    return (red >= 0 && white >= 0 && red + white <= 4);
+}
+
 void playGame(vector<Guess> & tries, int & total_red) {
     while (total_red != 4) {
         Guess g;
